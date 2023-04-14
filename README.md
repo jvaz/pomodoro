7a9e84059d5e72326565d909613733c2860baac9
Joao Vaz <castanheira.vaz@gmail.com>
2023-04-14 16:44:18 +0100

    full pomodoro cycle added

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
 # Pomodoro Timer
 
-A simple Pomodoro timer built with HTML, CSS, and JavaScript.
+A Pomodoro Timer is a simple time management technique that uses a timer to break down work into intervals, usually 25 minutes in length, separated by short breaks.
+
+This Pomodoro Timer is a web application built with HTML, CSS, and JavaScript, that allows users to set a timer for a Pomodoro session and take breaks in between. It has the following features:
 
 ## Features
-Set the timer for 25 minutes, the default length of a Pomodoro session.
-Click the "Start" button to begin the timer.
-The timer counts down from 25 minutes to 0.
-When the timer reaches 0, an alert pops up to indicate that the session is complete.
-Click the "Reset" button to reset the timer back to 25 minutes.
+
+    The timer can be set for a default of 25 minutes or for a custom duration.
+    Click the "Start" button to begin the timer.
+    The timer counts down from the set duration to 0.
+    When the timer reaches 0, an alert pops up to indicate that the session is complete.
+    A cycle consists of a work period and a break period. After a work period ends, a break period follows.
+    A session consists of multiple cycles. The default cycle is a 25 minute work period followed by a 5 minute break period. After 4 work cycles, a longer 15 minute break is taken.
+    The user can pause and resume the timer at any time during a session. The timer will continue from where it was paused.
+    The user can reset the timer at any time during a session. The timer will be reset to the beginning of the current cycle.
+    The user can switch to a dark mode for a more pleasant experience.
 
 ## Usage
-To use this Pomodoro timer, simply open the pomodoro.html file in your web browser.
 
-## Contributing
+To use this Pomodoro Timer, simply open the pomodoro.html file in your web browser.
+Contributing
 
 If you would like to contribute to this project, please fork the repository and submit a pull request.
 
 ## License
-his project is licensed under the MIT License. See the LICENSE file for more information.
+
+This project is licensed under the MIT License. See the LICENSE file for more information.
 
 ## Credits
-This Pomodoro timer was inspired by the work of Francesco Cirillo, creator of the Pomodoro Technique.
+
+This Pomodoro Timer was inspired by the work of Francesco Cirillo, creator of the Pomodoro Technique.
--- pomodoro.js
+++ pomodoro.js
@@ -1,67 +1,79 @@
-var minutes = 25;
-var seconds = 0;
-var timer;
-var isTimerRunning = false;
+$(document).ready(function () {
+  let interval;
+  let timeLeft = 0; // initialize timeLeft variable
+  let currentCycle = 1;
+  let cycles = [
+    { duration: 25, type: "Work" },
+    { duration: 5, type: "Short Break" },
+    { duration: 25, type: "Work" },
+    { duration: 5, type: "Short Break" },
+    { duration: 25, type: "Work" },
+    { duration: 5, type: "Short Break" },
+    { duration: 25, type: "Work" },
+    { duration: 15, type: "Long Break" },
+  ];
 
-function startTimer() {
-    // check if timer is not already running
-    if (!isTimerRunning) {
-        // set isTimerRunning to true
-        isTimerRunning = true;
-        // change text of start button to 'Stop'
-        $("#start-btn").text("Stop");
-        // set interval to run every second
-        timer = setInterval(function() {
-        // check if seconds have reached 0
-            if (seconds === 0) {
-                // check if minutes have reached 0
-                if (minutes === 0) {
-                    // if both minutes and seconds are 0, stop the timer and show alert message
-                    clearInterval(timer);
-                    alert("Time's up!");
-                    // set isTimerRunning to false
-                    isTimerRunning = false;
-                    // change text of start button to 'Start'
-                    $("#start-btn").text("Start");
-                    return;
-                }
-                // if seconds have reached 0 but minutes are not 0, decrease minutes by 1 and set seconds to 59
-                minutes--;
-                seconds = 59;
-            } else {
-                // if seconds are not 0, decrease seconds by 1
-                seconds--;
-            }
-            // update minutes and seconds in HTML
-            $(".minutes").text(minutes < 10 ? "0" + minutes : minutes);
-            $(".seconds").text(seconds < 10 ? "0" + seconds : seconds);
-      }, 1000); // set interval to run every 1000 milliseconds (1 second)
+  function startTimer(duration, display) {
+    let timer = duration || timeLeft, minutes, seconds;
+    interval = setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.querySelector(".minutes").textContent = minutes;
+      display.querySelector(".seconds").textContent = seconds;
+
+      if (--timer < 0) {
+        clearInterval(interval);
+        if (currentCycle === cycles.length) {
+          currentCycle = 1;
+        } else {
+          currentCycle++;
+        }
+        let nextCycle = cycles[currentCycle - 1];
+        display.querySelector(".cycle").textContent =
+          "Cycle " + currentCycle + " - " + nextCycle.type;
+        startTimer(nextCycle.duration * 60, display);
+      } else {
+        timeLeft = timer; // update timeLeft variable
+      }
+    }, 1000);
+  }
+
+  $("#start-btn").click(function () {
+    let display = document.querySelector(".timer");
+    let cycle = cycles[currentCycle - 1];
+    display.querySelector(".cycle").textContent =
+      "Cycle " + currentCycle + " - " + cycle.type;
+
+    if (interval) {
+      // timer already running, so pause it
+      clearInterval(interval);
+      interval = null;
     } else {
-        // if timer is already running, stop the timer, set isTimerRunning to false, and change text of start button to 'Start'
-        isTimerRunning = false;
-        $("#start-btn").text("Start");
-        clearInterval(timer);
+      // timer not running, so start it
+      startTimer(timeLeft || cycle.duration * 60, display);
     }
-}
-  
-function resetTimer() {
-    clearInterval(timer);
-    isTimerRunning = false;
-    minutes = 25;
-    seconds = 0;
-    $(".minutes").text("25");
-    $(".seconds").text("00");
-    $("#start-btn").text("Start");
-}
 
-$(document).ready(function() {
-  $("#start-btn").click(function() {
-    startTimer();
+    // toggle button text
+    $(this).toggleClass("btn-primary btn-danger");
+    $(this).text(interval ? "Stop" : "Start");
   });
-  $("#reset-btn").click(function() {
-    resetTimer();
+
+  $("#reset-btn").click(function () {
+    clearInterval(interval);
+    let display = document.querySelector(".timer");
+    display.querySelector(".cycle").textContent = "Cycle 1 - Work";
+    display.querySelector(".minutes").textContent = "25";
+    display.querySelector(".seconds").textContent = "00";
+    currentCycle = 1;
+    timeLeft = 0; // reset timeLeft variable
+    $("#start-btn").removeClass("btn-danger").addClass("btn-primary").text("Start"); // reset start button
   });
-  $("#dark-mode-btn").click(function() {
+
+  $("#dark-mode-btn").click(function () {
     $("body").toggleClass("dark-mode");
   });
 });
